60ace83f69cba38b78550c49271814b9b64a0c1d
Aastik Nayyar <aastiknayyar13@gmail.com>
2026-04-22 23:18:56 +0530

    completed

--- BookMyShow/Movie.java
+++ BookMyShow/Movie.java
@@ -1,5 +1,26 @@
-package BookMyShow;
+package MovieTicketBooking;
 
 public class Movie {
-    System.out.println("This is a movie class");
+
+    private String movieId;
+    private String movieTitle;
+    private int durationMinutes;
+
+    public Movie(String id, String title, int duration) {
+        this.movieId = id;
+        this.movieTitle = title;
+        this.durationMinutes = duration;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public String getTitle() {
+        return movieTitle;
+    }
+
+    public int getDuration() {
+        return durationMinutes;
+    }
 }
